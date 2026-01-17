3d1115f2016f480094d19db2af829eb65d35ea81
drocpdp <davidreynon@gmail.com>
2026-01-17 15:21:30 +0100

    test to create job and validate all fields

--- backend/tests/test_jobs.py
+++ backend/tests/test_jobs.py
@@ -5,6 +5,7 @@ import json
 from sqlalchemy import text
 
 from utils.strings import get_unique_s
+from utils.dates import make_date
 
 def test_create_job_and_get_by_id(client):
     t_company = "company-" + get_unique_s()
@@ -25,6 +26,36 @@ def test_create_job_and_get_by_id(client):
     job2 = r2.json()
     assert job2["id"] == job_id
     assert job2["company"] == t_company
+
+def test_create_job_validate_all_fields(client):
+    t_company = "company-" + get_unique_s()
+    t_title = "title-" + get_unique_s()
+    t_location = "location-" + get_unique_s()
+    t_start_date = make_date(year=2022, month=5, day=23)
+    t_end_date = make_date(year=2022, month=6, day=25)
+    t_summary = "summary-" + get_unique_s()
+    t_notes = "notes-" + get_unique_s()
+
+    job_payload = {
+        "company": t_company,
+        "title": t_title,
+        "location": t_location,
+        "start_date": t_start_date,
+        "end_date": t_end_date,
+        "summary": t_summary,
+        "notes": t_notes
+    }
+
+    r = client.post("/jobs", json=job_payload)
+    assert r.status_code == 201
+    job = r.json()
+    assert job["company"] == t_company
+    assert job["title"] == t_title
+    assert job["location"] == t_location
+    assert job["start_date"] == t_start_date
+    assert job["end_date"] == t_end_date
+    assert job["summary"] == t_summary
+    assert job["notes"] == t_notes
 
 def test_create_job_missing_title(client):
     t_company = "company-" + get_unique_s()
--- backend/tests/utils/dates.py
+++ backend/tests/utils/dates.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, date
+from typing import Optional
 
 
 def get_epoch_time_s():
@@ -6,3 +7,17 @@ def get_epoch_time_s():
     Get unique string based on current epoch time
     """
     return datetime.now().timestamp()
+
+def make_date(
+    *,
+    year: int,
+    month: Optional[int] = 1,
+    day: Optional[int] = 1,
+) -> str:
+    """
+    Examples:
+        make_date(year=2022)
+        make_date(year=2022, month=6)
+        make_date(year=2022, day=15)
+    """
+    return date(year, month or 1, day or 1).isoformat()
