e24724192cee689a14187acb7e60f41d5b7a380b
drocpdp <davidreynon@gmail.com>
2026-01-25 14:07:58 +0100

    typical tests for jobs

--- backend/tests/test_jobs.py
+++ backend/tests/test_jobs.py
@@ -7,7 +7,7 @@ from sqlalchemy import text
 from utils.strings import get_unique_s, create_field
 from utils.dates import make_date
 
-from utils.api import create_new_job, post_create_job
+from utils.api import create_new_job, post_create_job, create_complete_test_job
 
 def test_create_new_job(client):
     t_company = create_field("company")
@@ -101,3 +101,81 @@ def test_create_job_missing_title_and_company(client):
     assert r.status_code == 422
     content_detail = json.loads(r.content)["detail"][0]["msg"]
     assert content_detail == "Field required"
+
+
+def test_get_jobs_validate_returned_list_of_all_jobs(client):
+    jobs = {}
+
+    num_tests = 4
+
+    for _ in range(num_tests):
+        job = create_complete_test_job(client)
+        job_id = job["id"]
+        company_name = job["company"]
+
+        jobs["id"] = [company_name, job]
+
+    # Get all jobs
+    r_get = client.get(f"/jobs/")
+    assert r_get.status_code == 200
+
+    jobs_get = r_get.json()
+
+    assert len(jobs_get) == num_tests
+
+    jobs_by_company_names = {job["company"]: job for job in jobs_get}
+
+    unique_job_company_names = {company_name for company_name in jobs_by_company_names}
+
+    assert len(unique_job_company_names) == num_tests
+
+
+def test_get_jobs_with_similar_company_name_string_expect_set_of_results(client):
+    jobs = {}
+
+    num_tests = 4
+
+    common_string = "compa" # all jobs have 'company' within
+
+    for _ in range(num_tests):
+        job = create_complete_test_job(client)
+        job_id = job["id"]
+        job_company_name = job["company"]
+
+        jobs[job_id] = [job_company_name, job]
+
+    # Get by common name
+    r_get = client.get(f"/jobs/", params={"q":common_string})
+    assert r_get.status_code == 200
+
+    job_get = r_get.json()
+
+    assert len(job_get) == num_tests
+
+    jobs_by_company_names = {job["company"]: job for job in job_get}
+
+    for name in jobs_by_company_names:
+        assert common_string in name
+
+
+def test_get_jobs_with_non_existent_company_name_expect_no_results(client):
+    jobs = {}
+
+    num_tests = 4
+
+    common_string = "XXX"
+
+    for _ in range(num_tests):
+        job = create_complete_test_job(client)
+        job_id = job["id"]
+        job_company_name = job["company"]
+
+        jobs[job_id] = [job_company_name, job]
+
+    # Get by common name
+    r_get = client.get(f"/jobs/", params={"q":common_string})
+    assert r_get.status_code == 200
+
+    job_get = r_get.json()
+
+    assert len(job_get) == 0
